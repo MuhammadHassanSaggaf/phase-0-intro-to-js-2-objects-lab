bf5e6042937814e5bce1badd7f8cb9503ba08a16
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-06-14 14:09:24 +0300

    Wrote object functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+const employee = {
+  name : 'Muhammad Saggaf',
+  address : 'Mombasa'
+
+};
+
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+  return {
+    ...emp,
+    [key] : value
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+  emp[key] = value;
+  return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key){
+  let newEmp = {...emp};
+  delete newEmp[key];
+  return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key){
+  delete emp[key];
+  return emp;
+}
